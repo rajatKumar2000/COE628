624bc165112b6988e1a91888cae4184eb9a14269
smyousuf14 <44485548+smyousuf14@users.noreply.github.com>
2020-02-03 16:12:29 -0500

    Add files via upload

---
+++ lab4/main.c
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: smyousuf
+ *
+ * Created on February 3, 2020, 3:42 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    system("wc -w build  dist  junk  main.c  Makefile  nbproject ");
+    
+    
+    return (EXIT_SUCCESS);
+}
+
