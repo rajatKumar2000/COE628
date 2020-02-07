491ab76acdc294a971d10ab0d5bc4bee29f7ceee
smyousuf14 <44485548+smyousuf14@users.noreply.github.com>
2020-02-07 12:47:05 -0500

    Add files via upload

---
+++ lab4/lab4/main.c
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: smyousuf
+ *
+ * Created on February 3, 2020, 3:42 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h> 
+#include <fcntl.h>
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    int     fd[2], nbytes;
+        pid_t   childpid;
+        char    string[] = "WWW\n";
+        char    readbuffer[80];
+
+        pipe(fd);
+        
+        if((childpid = fork()) == -1)
+        {
+                perror("fork");
+                exit(1);
+        }
+
+        if(childpid == 0)
+        {
+                /* Child process closes up input side of pipe */
+                close(fd[0]);
+
+                /* Send "string" through the output side of pipe */
+                write(fd[1], string, (strlen(string)+1));
+                exit(0);
+        }
+        else
+        {
+                /* Parent process closes up output side of pipe */
+                close(fd[1]);
+
+                /* Read in a string from the pipe */
+                nbytes = read(fd[0], readbuffer, sizeof(readbuffer));
+                printf("Received string: %s", readbuffer);
+        }
+    
+    
+    //system("rm temp.txt");
+    return (EXIT_SUCCESS);
+}
+
